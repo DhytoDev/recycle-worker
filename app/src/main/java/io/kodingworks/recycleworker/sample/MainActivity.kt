93fcc5d34120f7c2003fd72995f6d6b177db7f72
Fordyta Abubakar <dhytodev@gmail.com>
2020-01-16 21:03:38 +0800

    add sample

--- app/src/main/java/io/kodingworks/recycleworker/sample/MainActivity.kt
+++ app/src/main/java/io/kodingworks/recycleworker/sample/MainActivity.kt
@@ -1,12 +1,37 @@
 package io.kodingworks.recycleworker.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import io.kodingworks.recycleworker.ViewBinding
+import io.kodingworks.recycleworker.setupAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.layout_rv_item.view.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ViewBinding<Item> {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val items = listOf(Item("Abubakar", "Pallangga"), Item("Dhyto", "Jental"))
+
+        recyclerView.setupAdapter(R.layout.layout_rv_item, this)
+            .setLayoutManager(LinearLayoutManager(this))
+            .submitList(items)
+            .create()
     }
+
+    override fun bind(view: View, position: Int, item: Item?) {
+        item?.apply {
+            view.name.text = name
+            view.address.text = address
+        }
+    }
+
+
 }
+
+data class Item(val name: String, val address: String)
